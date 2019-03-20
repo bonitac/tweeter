3000a1f9c46929895c0a372ff5b3e080f83370d4
Bonita Choi <bonitac93@gmail.com>
2019-03-20 23:26:25 +0000

    figured out why so many tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,43 +9,40 @@ function renderTweets(tweets) {
   tweets.forEach(function(tweet){
     tweetContainer.append(createTweetElement(tweet));
   });
+  return tweetContainer;
 }
 
 
 function createTweetElement(tweet) {
   let $tweet = $('<article>').addClass('tweets');
   let $header = $('<header>').addClass('user-info');
-  // let $avatar = ;
-  // let $name = ;
-  // let $handle = ;
-  let $tweetContent = $('<h5>').addClass('tweet-content').text(tweet.content.text);
   let $time = $("<footer>").addClass('time').text(tweet.created_at)
   $tweet.append($header);
   $header.append($('<img>').addClass('avatar').attr("src", tweet.user.avatars.small));
   $header.append($('<h4>').addClass('handle').text(tweet.user.handle));
   $header.append($('<h3>').addClass('name').text(tweet.user.name));
-  $tweet.append($tweetContent);
+  $tweet.append($('<h5>').addClass('tweet-content').text(tweet.content.text));
   $tweet.append($time); //need to convert time
   return $tweet;
 }
 
 function loadTweets(){
   $('.tweets-container').empty()
-  $.ajax('http://localhost:8080/tweets', { method: 'GET' })
+  $.ajax('/tweets', { method: 'GET' })
    .then((response) => {
      renderTweets(response);
     });
 }
 
 $(document).ready(function() {
-  loadTweets();
+  // loadTweets();
   $('#new-tweet').submit(function (e){
     e.preventDefault();
     const serializedTweet = $(this).serialize();
     $.post('/tweets',serializedTweet,(res)=>{
-      createTweetElement(serializedTweet);
+      // console.log(serializedTweet)
+      // console.log(res);
+      loadTweets();
     })
-    tweets.push($(':input').val())
-    renderTweets(tweets);
   })
 })
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,6 @@ const maxChar = 140;
 $(document).ready(function() {
   $(":input").change(function(){
     $("span.counter").text(maxChar-$(":input").val().length)
-    event.preventDefault();
   })
-  event.preventDefault();
+  
 })
